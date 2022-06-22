06de1392d50c7390f511dec187889f16a67555af
暗语寒飞 <1223050251@qq.com>
2022-06-22 13:39:16 +0800

    兼容性问题

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -36,7 +36,7 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
 
-            Route::prefix('api')->group(base_path('app\Api\routes.php'));
+            Route::prefix('api')->group(base_path('app/Api/routes.php'));
         });
     }
 
